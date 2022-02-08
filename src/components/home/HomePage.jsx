26f8e4f47842a6976efb2408b08442ed336335f9
90150N <mugirethe1@gmail.com>
2022-02-08 23:34:01 +0300

    style: rename empty tags to section

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -2,14 +2,14 @@ import "./home.css";
 
 function HomePage() {
   return (
-    <>
+    <section className="banner">
       <h2>Choked By Your Trash?</h2>
       <p>We are here to help you dispose it !</p>
       <div className="searchBar">
         <input type="text" name="search" id="search" placeholder="Enter your Location" />
         <button type="submit">Search</button>
       </div>
-    </>
+    </section>
   );
 }
 
