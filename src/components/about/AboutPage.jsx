5e2c86cb597049abcf9792c7caaa61561848194e
IanoSang <ianosang18@gmail.com>
2022-02-09 12:58:58 +0300

    patch: design about us component

--- src/components/about/AboutPage.jsx
+++ src/components/about/AboutPage.jsx
@@ -1,6 +1,49 @@
+import "./about.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRecycle, faTruck, faAnglesRight } from "@fortawesome/free-solid-svg-icons";
+
 function AboutPage(){
-    return(
-        <h2>About Page</h2>
+    return (
+      <div className="container">
+        <div className="about">
+          <h2>About Us</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo, a.
+            Alias perspiciatis neque voluptatem quam dolorem fuga, fugiat
+            perferendis natus mollitia eum odio tempore? Enim eius ipsa
+            voluptatem ipsam accusamus.
+          </p>
+        </div>
+        <div className="about-boxes">
+          <div>
+            <FontAwesomeIcon className="recycle" icon={faRecycle} />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro
+              deleniti numquam expedita cumque accusantium iusto magnam, nemo in
+              quam amet aliquam ea ut quasi dolore placeat? Labore dolores error
+              necessitatibus.
+            </p>
+          </div>
+          <div>
+            <FontAwesomeIcon className="recycle" icon={faTruck} />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro
+              deleniti numquam expedita cumque accusantium iusto magnam, nemo in
+              quam amet aliquam ea ut quasi dolore placeat? Labore dolores error
+              necessitatibus.
+            </p>
+          </div>
+          <div>
+            <FontAwesomeIcon className="recycle" icon={faAnglesRight} />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro
+              deleniti numquam expedita cumque accusantium iusto magnam, nemo in
+              quam amet aliquam ea ut quasi dolore placeat? Labore dolores error
+              necessitatibus.
+            </p>
+          </div>
+        </div>
+      </div>
     );
 }
 
